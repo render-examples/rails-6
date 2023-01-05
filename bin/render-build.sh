f38397c5449460b989603bd0e4024f692302cd6b
Anurag Goel <anurag@render.com>
2023-01-04 17:12:53 -0800

    Use system libs for nokogiri

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,9 @@
 # exit on error
 set -o errexit
 
+# the current version of Nokogiri ships with incompatible libs
+bundle config --local build.nokogiri --use-system-libraries
+
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
